f977f075c2ca9142027c7572a155b42674a3e703
Leo Rodriguez <leorodriguez246@gmail.com>
2026-08-20 12:38:32 -0400

    Subida de proyecto basico
    Proyecto inicial

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//Se escribe en consola el titulo
+Console.WriteLine("=== Sumatoria de Numeros ===");
+
+//Pedimos valores al usuario
+Console.Write("Ingrese un numero: ");
+var entrada1 = Console.ReadLine(); // Captura como un String
+
+//Pedimos otro valor al usuario
+Console.Write("Ingrese otro numero: ");
+var entrada2 = Console.ReadLine();
+
+if (double.TryParse(entrada1, out double n1) && double.TryParse(entrada2, out double n2))
+{
+    var suma = n1 + n2;
+    Console.WriteLine($"La Suma final es = {suma}");
+}
+else
+{
+    Console.WriteLine("Hubo un error en la suma, verifique que valores ingreso.");
+}
+
+Console.WriteLine("\nPresione cualquier tecla para finalizar...");
+Console.ReadKey();
+
+
+
+
+
+
